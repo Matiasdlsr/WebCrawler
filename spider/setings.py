8d7a79f65a111da55c63d49c3fe672fd3f56ec11
matiasdlsr <166665626+Matiasdlsr@users.noreply.github.com>
2024-11-02 02:14:31 -0300

    :new: extraction

--- spider/setings.py
+++
@@ -1,7 +0,0 @@
-#?
-REQUEST_FINDERPRINTER_IMPLEMENTATION = "2.7"
-TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsincioSelectorReactor"
-FEED_EXPORT_ENCODING = "utf-8" #codificacion de la exportacion 
-ITEM_PIPELINES={
-    'spider.pipelines.JsonWriterPipeline' : 300, #activacion de propiedad para guardar json
-}
